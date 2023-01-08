f5d434129636c17fc33fe1202dd7096c7585b890
H123bee <little___bee@sina.com>
2023-01-08 13:08:58 +0800

    d

---
+++ website/js/index.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+		//let ele=document.getElementByName('user');
+		//let s=document.getElementById('submit-box');
+		//if(ele!="2004")
+		/*s.onclick=function(){
+			alert("账号错误!");
+		}*/
+
+}
